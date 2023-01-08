52e6df6ba1ce2a6bd602be270a5b5488f95a8731
MAKIENAUT <mcrayescoto@gmail.com>
2023-01-08 14:37:31 +0800

    Push existing project to GitLab

--- form/interest_form.php
+++ form/interest_form.php
@@ -191,7 +191,9 @@
                <div class="upload" id="upload">
                   <label id="upload-info">4. Document Upload *</label>
 
-                  <label id="valid-title1">!!! Please upload PDF format of files.</label>
+                  <label id="valid-title1">
+                     !!! The form only accepts .PDF file format input.
+                  </label>
                   <label id="valid-title">Resume / Curriculum Vitae *</label>
                   <label for="resume" class="valid-input" id="resume_label">
                      Resume / Curriculum Vitae
